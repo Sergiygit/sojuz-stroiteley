9f57a12937c507f8e365fad8a4d5f7a3d2723176
Serhii <102955337+Sergiygit@users.noreply.github.com>
2026-01-14 20:48:37 +0200

    Add files via upload

--- js/dev/managment.js
+++ js/dev/managment.js
@@ -1,25 +1,36 @@
 import "./footer.min.js";
 /* empty css           */
 import "./common.min.js";
-let element = document.querySelectorAll(".marker");
+let marker = document.querySelectorAll(".marker");
 function disActiveMapPopUp(el) {
   let disActive = document.querySelectorAll(".map-popup");
-  disActive.forEach((element2) => {
-    if (el != element2.id) {
-      element2.style.display = "none";
+  disActive.forEach((element) => {
+    if (el != element.id) {
+      element.style.display = "none";
     }
   });
 }
-element.forEach((e) => {
+function popupActive(id, x, y) {
+  let left = x;
+  let top = y;
+  const popup = document.getElementById(id);
+  if (id == "batken" & window.innerWidth < 1200) {
+    popup.children[1].style.transform = "translate(-50px)";
+  }
+  if (id == "issyk-kul" & window.innerWidth < 1200) {
+    popup.children[1].style.transform = "translate(-120px)";
+  }
+  popup.style.left = `${left + window.scrollX}px`;
+  popup.style.top = `${top + window.scrollY}px`;
+  document.querySelector(`.${id}`).style.display = "block";
+}
+marker.forEach((e) => {
   e.addEventListener("click", function(e2) {
     let elementId = this.getAttribute("data-region");
     let markersValue = this.children.length;
     let activeMarker = this.children[markersValue - 1];
     const rect = activeMarker.getBoundingClientRect();
-    const popup = document.getElementById(elementId);
-    popup.style.left = `${rect.left + window.scrollX}px`;
-    popup.style.top = `${rect.top + window.scrollY}px`;
-    document.querySelector(`.${elementId}`).style.display = "block";
+    popupActive(elementId, rect.left, rect.top);
     disActiveMapPopUp(elementId);
   });
 });
